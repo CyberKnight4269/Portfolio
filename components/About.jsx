b4122296e5878ff28701cb6676cae244aed5a88e
Divyansh kanaujia <kanaujiadivyansh@gmail.com>
2024-11-01 23:53:18 +0530

    first change in newDesign

--- components/About.jsx
+++ components/About.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-// import Image from ''
+import Header from './Header.jsx'
 
 function About() {
   return (
-    <section id="about" className="container">
-        <h2>About Me</h2>
-        <img src="https://media.licdn.com/dms/image/v2/D5603AQFo0jHkjvVNSg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724741142675?e=1730332800&v=beta&t=HJhX4r2OZzmdL_GTPqk72QT1_WQvC-VegYBCFqB1aOg" alt="Divyansh" />
-        {/* <Image/> */}
-        <p>
-            Hi, I'm <span className="highlight">Divyansh Kanaujia</span>, a passionate Computer Science student with a love for building innovative solutions.
-            <br /><br />
-            I specialize in <span className="highlight">Full-Stack Development</span>, working with technologies like the MERN stack (MongoDB, Express.js, React, Node.js). I'm committed to creating user-friendly and efficient applications that solve real-world problems.
-            <br />
-        </p>
-    </section>
+    <div>
+        <section id="about" className="container">
+            <img src="https://media.licdn.com/dms/image/v2/D5603AQFo0jHkjvVNSg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724741142675?e=1730332800&v=beta&t=HJhX4r2OZzmdL_GTPqk72QT1_WQvC-VegYBCFqB1aOg" alt="Divyansh" />
+            <p>
+                Hi, I'm <span className="highlight">Divyansh Kanaujia</span>, a passionate Computer Science student with a love for building innovative solutions.
+                <br /><br />
+                I specialize in <span className="highlight">Full-Stack Development</span>, working with technologies like the MERN stack (MongoDB, Express.js, React, Node.js). I'm committed to creating user-friendly and efficient applications that solve real-world problems.
+                <br />
+            </p>
+        </section>
+    </div>
   )
 }
 
