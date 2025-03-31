f2ae7e9e47a93af242e230c433f1e152d7b3cbac
Divyansh kanaujia <kanaujiadivyansh@gmail.com>
2025-03-31 18:09:40 +0530

    Added project 3

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -13,10 +13,10 @@ function Projects() {
                 <h3 className="project-title">MelodyServer</h3>
                 <p className="project-description">MelodyServer is a backend server for a music application, built to handle user authentication and user interactions, music streaming, playlists, and more. It features a RESTful API for seamless integration, efficient data handling, and scalability to support rich music experiences.</p>
             </a>
-            {/* <a href='' className="project-item">
-                <h3 className="project-title">Project 3</h3>
-                <p className="project-description">Project 3 Description</p>
-            </a> */}
+            <a href='https://github.com/CyberKnight4269/MNIST-ANN' className="project-item">
+                <h3 className="project-title">MNIST-ANN</h3>
+                <p className="project-description">An Artificial Neural Network model to predict hand-written digits using the MNIST dataset. Implemented in TensorFlow Keras on Google Colab.</p>
+            </a>
         </div>
     </section>
   )
